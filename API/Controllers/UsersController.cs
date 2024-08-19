ee829e1138d54ae0c057d7ec91bd1f01c4c6853c
Mirko Karac <mirkokarac@hotmail.com>
2024-08-19 15:39:49 +1000

    Create UsersController.cs
    Add users and user endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // /api/users
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")] // /api/users/1
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = context.Users.Find(id);
+
+            if (user == null) return NotFound();
+
+            return user;
+        }
+    }
+}
