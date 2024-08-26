e900365cd3f2241e1afb4e43846a2723f7af60ea
Mirko Karac <mirkokarac@hotmail.com>
2024-08-26 11:38:20 +1000

    Refactor to use get member(s) tasks and use automapper queryable extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,25 +7,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository,
-        IMapper mapper) : BaseApiController
+    public class UsersController(IUserRepository userRepository) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await userRepository.GetUsersAsync();
-            var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
-            return Ok(usersToReturn);
+            var users = await userRepository.GetMembersAsync();
+            return Ok(users);
         }
 
         [HttpGet("{username}")] // /api/users/1
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await userRepository.GetUserByUsernameAsync(username);
+            var user = await userRepository.GetMemberAsync(username);
 
             if (user == null) return NotFound();
 
-            return mapper.Map<MemberDto>(user);
+            return user;
         }
     }
 }
