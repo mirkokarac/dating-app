2d7acf8d5864c187ebc55971f7856909cb1aa717
Mirko Karac <mirkokarac@hotmail.com>
2024-08-26 11:12:32 +1000

    Refactor user controller to use auto mapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,28 +1,31 @@
-using API.Entities;
+using API.DTOs;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository,
+        IMapper mapper) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await userRepository.GetUsersAsync();
-            return Ok(users);
+            var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{username}")] // /api/users/1
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             var user = await userRepository.GetUserByUsernameAsync(username);
 
             if (user == null) return NotFound();
 
-            return user;
+            return mapper.Map<MemberDto>(user);
         }
     }
 }
