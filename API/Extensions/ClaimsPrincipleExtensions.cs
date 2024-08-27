dece1a8cd1fb8b38e47fb92b2bb91d9f365ba2c0
Mirko Karac <mirkokarac@hotmail.com>
2024-08-27 17:00:54 +1000

    Add get username extension

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,13 @@
+using System.Security.Claims;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipleExtensions
+{
+    public static string GetUserName(this ClaimsPrincipal user)
+    {
+        var userName = user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("Cannot get username from token");
+        return userName;
+    }
+}
