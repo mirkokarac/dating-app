66ee20887e079d18844bd0a40685972bc39f20b1
Mirko Karac <mirkokarac@hotmail.com>
2024-09-10 11:20:06 +1000

    Updating the account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,16 +1,15 @@
 using System.Security.Cryptography;
-using System.Text;
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context,
+    public class AccountController(UserManager<AppUser> userManager,
         ITokenService tokenService, IMapper mapper) : BaseApiController
     {
         [HttpPost("register")] // account/register
@@ -18,13 +17,12 @@ namespace API.Controllers
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-            using var hmac = new HMACSHA512();
-
             var user = mapper.Map<AppUser>(registerDto);
             user.UserName = registerDto.Username.ToLower();
 
-            context.Users.Add(user);
-            await context.SaveChangesAsync();
+            var result = await userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
             return new UserDto
             {
@@ -38,12 +36,16 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await context.Users
+            var user = await userManager.Users
                 .Include(p => p.Photos)
                 .FirstOrDefaultAsync(x =>
-                x.UserName == loginDto.Username.ToLower());
+                x.NormalizedUserName == loginDto.Username.ToUpper());
 
             if (user == null || user.UserName == null) return Unauthorized("Invalid username");
+
+            var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result) return Unauthorized();
 
             return new UserDto
             {
@@ -57,7 +59,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await context.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper());
+            return await userManager.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper());
         }
     }
 }
