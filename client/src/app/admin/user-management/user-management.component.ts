cb92cefa45de5620adf27c933d75ea657e9c1209
Mirko Karac <mirkokarac@hotmail.com>
2024-09-10 14:41:52 +1000

    Setting up modals

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AdminService } from '../../_services/admin.service';
 import { User } from '../../_models/user';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.component';
+import { initialState } from 'ngx-bootstrap/timepicker/reducer/timepicker.reducer';
 
 @Component({
   selector: 'app-user-management',
@@ -11,10 +14,24 @@ import { User } from '../../_models/user';
 })
 export class UserManagementComponent implements OnInit {
   private adminService = inject(AdminService);
+  private modalService = inject(BsModalService);
   users: User[] = [];
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
   ngOnInit(): void {
     this.getUsersWithRoles();
+  }
+
+  openRolesModal() {
+    const initialState: ModalOptions = {
+      class: 'modal-lg',
+      initialState: {
+        title: 'User roles',
+        list: ['Admin', 'Moderator', 'Member']
+      }
+    }
+
+    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
   }
 
   getUsersWithRoles() {
