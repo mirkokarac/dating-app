eea1618b992c11c7ee2f79459bdbe5c8b1c127e1
Mirko Karac <mirkokarac@hotmail.com>
2024-09-11 13:18:17 +1000

    Adding a presence tracker

---
+++ API/SignalR/PresenceTracker.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace API.SignalR;
+
+public class PresenceTracker
+{
+    public static readonly Dictionary<string, List<string>> OnlineUsers = [];
+
+    public Task UserConnected(string username, string connectionId)
+    {
+        lock (OnlineUsers)
+        {
+            if (OnlineUsers.ContainsKey(username))
+            {
+                OnlineUsers[username].Add(connectionId);
+            }
+            else
+            {
+                OnlineUsers.Add(username, [connectionId]);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task UserDisconnected(string username, string connectionId)
+    {
+        lock (OnlineUsers)
+        {
+            if (!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+
+            OnlineUsers[username].Remove(connectionId);
+
+            if (OnlineUsers[username].Count == 0)
+            {
+                OnlineUsers[username].Remove(username);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string[]> GetOnlineUsers()
+    {
+        string[] onlineUsers;
+
+        lock (OnlineUsers)
+        {
+            onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+        }
+
+        return Task.FromResult(onlineUsers);
+    }
+}
