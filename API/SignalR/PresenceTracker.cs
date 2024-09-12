92c8c01a84bb2a3be2266aea855ae215aa467c1f
Mirko Karac <mirkokarac@hotmail.com>
2024-09-12 16:11:27 +1000

    Optimizing the presence

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -6,8 +6,10 @@ public class PresenceTracker
 {
     public static readonly Dictionary<string, List<string>> OnlineUsers = [];
 
-    public Task UserConnected(string username, string connectionId)
+    public Task<bool> UserConnected(string username, string connectionId)
     {
+        var isOnline = false;
+
         lock (OnlineUsers)
         {
             if (OnlineUsers.ContainsKey(username))
@@ -17,27 +19,31 @@ public class PresenceTracker
             else
             {
                 OnlineUsers.Add(username, [connectionId]);
+                isOnline = true;
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOnline);
     }
 
-    public Task UserDisconnected(string username, string connectionId)
+    public Task<bool> UserDisconnected(string username, string connectionId)
     {
+        var isOffline = false;
+
         lock (OnlineUsers)
         {
-            if (!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+            if (!OnlineUsers.ContainsKey(username)) return Task.FromResult(isOffline);
 
             OnlineUsers[username].Remove(connectionId);
 
             if (OnlineUsers[username].Count == 0)
             {
                 OnlineUsers[username].Remove(username);
+                isOffline = true;
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOffline);
     }
 
     public Task<string[]> GetOnlineUsers()
