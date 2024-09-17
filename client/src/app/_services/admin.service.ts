09be06ccdeeaa28f2b54596009ce94d295950a62
Mirko Karac <mirkokarac@hotmail.com>
2024-09-17 17:29:49 +1000

    Add photo management client side

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -2,20 +2,33 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../_models/user';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  baseURL = environment.apiUrl;
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
 
   getUserWithRoles() {
-    return this.http.get<User[]>(this.baseURL + 'admin/users-with-roles');
+    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
   }
 
   updateUserRoles(username: string, roles: string[]) {
-    return this.http.post<string[]>(this.baseURL + 'admin/edit-roles/'
+    return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/'
       + username + '?roles=' + roles, {});
+  }
+
+  getPhotosForApproval() {
+    return this.http.get<Photo[]>(this.baseUrl + 'admin/photos-to-moderate');
+  }
+
+  approvePhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/approve-photo/' + photoId, {});
+  }
+
+  rejectPhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/reject-photo/' + photoId, {});
   }
 }
